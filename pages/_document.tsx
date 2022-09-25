b7d3be6a8c59c87692d5f31b6de1b5f07c4aa3ff
Renat <restspread@gmail.com>
2022-09-25 17:41:14 +0300

    feature add fonts, css variables

---
+++ pages/_document.tsx
@@ -0,0 +1,26 @@
+import Document, { Html, Head, Main, NextScript, DocumentContext, DocumentInitialProps } from 'next/document';
+
+class MyDoc extends Document {
+
+	static async getInitialProps(ctx: DocumentContext): Promise<DocumentInitialProps> {
+		const initialProps = await Document.getInitialProps(ctx);
+		return {...initialProps};
+	}
+
+	render() {
+		return (
+			<Html lang='ru'>
+				<Head>
+					<link rel="preconnect" href="https://fonts.googleapis.com" />
+					<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Noto+Sans:wght@300;400;500;700&display=swap" rel="stylesheet" />
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDoc;
