3c90d91fd7514e260bea962198c54c55a2a91ba7
Renat <restspread@gmail.com>
2022-10-01 18:47:06 +0300

    feature add component P

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { Htag, Button } from "../components";
+import { Htag, Button, P } from "../components";
 
 export default function Home(): JSX.Element {
   return (
@@ -7,6 +7,9 @@ export default function Home(): JSX.Element {
 			<Button appearance="primary" arrow="right">Узнать подробнее</Button>
 			<Button appearance="ghost" arrow="down">Узнать подробнее</Button>
 			<Button appearance="form" arrow="none">Узнать подробнее</Button>
+			<P size="s">adasdasdaddsad</P>
+			<P>adasdasdaddsad</P>
+			<P size="l">adasdasdaddsad</P>
 		</>
   );
 }
