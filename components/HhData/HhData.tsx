64e744cd099efb09dc6e395c1ffb45369ad2e77a
Renat <restspread@gmail.com>
2022-10-30 21:52:24 +0300

    feature add salaryRu

--- components/HhData/HhData.tsx
+++ components/HhData/HhData.tsx
@@ -3,6 +3,7 @@ import styles from './HhData.module.css';
 import cn from 'classnames';
 import StarIcon from "./star-icon.svg";
 import { Card } from "../Card/Card";
+import { salaryRu } from "../../helpers/helpers";
 
 export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDataProps):JSX.Element => {
 	return (
@@ -14,7 +15,7 @@ export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDa
 			<Card className={styles.salary}>
 				<div className={styles.salaryCard}>
 					<div className={styles.level}>Начальный</div>
-					<div className={styles.salaryValue}>{ juniorSalary }</div>
+					<div className={styles.salaryValue}>{ salaryRu(juniorSalary) }</div>
 					<div className={styles.rating}>
 						<StarIcon className={styles.filled} />
 						<StarIcon />
@@ -23,7 +24,7 @@ export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDa
 				</div>
 				<div className={styles.salaryCard}>
 					<div className={styles.level}>Средний</div>
-					<div className={styles.salaryValue}>{ middleSalary }</div>
+					<div className={styles.salaryValue}>{salaryRu(middleSalary) }</div>
 					<div className={styles.rating}>
 						<StarIcon className={styles.filled} />
 						<StarIcon className={styles.filled} />
@@ -32,7 +33,7 @@ export const HhData = ({ count, juniorSalary, middleSalary, seniorSalary }: HhDa
 				</div>
 				<div className={styles.salaryCard}>
 					<div className={styles.level}>Профессионал</div>
-					<div className={styles.salaryValue}>{ seniorSalary }</div>
+					<div className={styles.salaryValue}>{ salaryRu(seniorSalary) }</div>
 					<div className={styles.rating}>
 						<StarIcon className={styles.filled} />
 						<StarIcon className={styles.filled} />
--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -5,9 +5,13 @@ import BooksIcon from "./icons/books.svg";
 import ProductsIcon from "./icons/products.svg";
 import ServicesIcon from "./icons/services.svg";
 
+/* Menu.tsx */
 export const firstLevelMenu: FirstLevelMenuItem[] = [
 	{ route: "courses", name: "Курсы", icon: <CoursesIcon />, id: TopLevelCategory.Courses},
 	{ route: "services", name: "Сервисы", icon: <ServicesIcon />, id: TopLevelCategory.Services},
 	{ route: "books", name: "Книги", icon: <BooksIcon />, id: TopLevelCategory.Books},
 	{ route: "products", name: "Товары", icon: <ProductsIcon />, id: TopLevelCategory.Products}
 ];
+
+/* HhData.tsx */
+export const salaryRu = (value: number) => value.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ') + ' ₽';
