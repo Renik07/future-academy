b26f12cb6dc6f94bedf8203de742615dd9df8ff9
Renat <restspread@gmail.com>
2022-11-16 00:00:43 +0300

    feature add Header component

--- layouts/Header/Header.tsx
+++ layouts/Header/Header.tsx
@@ -1,11 +1,44 @@
 import { HeaderProps } from "./Header.props";
-/* import styles from './P.module.css';
-import cn from 'classnames'; */
+import styles from './Header.module.css';
+import Logo from './logo.svg';
+import CoursesIcon from './courses.svg';
+import UserIcon from './user.svg';
 
 export const Header = ({ ...props }: HeaderProps):JSX.Element => {
 	return (
-		<header {...props}>
-			Header
+		<header 
+			className={styles.header} 
+			{...props}
+		>
+			<div className={styles.container}>
+			<Logo />
+			<nav className={styles.nav}>
+				<ul className={styles.list}>
+					<li className={styles.item}>
+						<a href="">
+							<CoursesIcon />
+							Все курсы
+						</a>
+					</li>
+					<li className={styles.item}>
+						<a href="">Мероприятия</a>
+					</li>
+					<li className={styles.item}>
+						<a href="">Базы знаний</a>
+					</li>
+					<li className={styles.item}>
+						<a href="">Карьера</a>
+					</li>
+				</ul>
+			</nav>
+			<a className={styles.tel} href="tel:+78009503398">
+				8 800 950-33-98 
+			</a>
+			<a className={styles.enter} href="">
+				<UserIcon />
+				Войти
+			</a>
+			</div>
 		</header>
-	)
+	);
 };
