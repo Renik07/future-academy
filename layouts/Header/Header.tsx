dce7cf48cbc3d2d842db6c5b9fe0a2698bdd9c26
Renat <restspread@gmail.com>
2022-11-26 21:21:52 +0300

    feature add mobile sidebar, add animation

--- layouts/Header/Header.tsx
+++ layouts/Header/Header.tsx
@@ -3,8 +3,49 @@ import styles from './Header.module.css';
 import Logo from './logo.svg';
 import CoursesIcon from './courses.svg';
 import UserIcon from './user.svg';
+import { ButtonIcon } from "../../components";
+import { Sidebar } from "../Sidebar/Sidebar";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 export const Header = ({ ...props }: HeaderProps):JSX.Element => {
+	const [isOpenedMenu, setIsOpenedMenu] = useState<boolean>(false);
+	const router = useRouter();
+
+	useEffect(() => {
+		setIsOpenedMenu(false);
+	}, [router]);
+
+	const variants = {
+		closedMenu: {
+			x: '-100%',
+			transition: {
+				duration: 1
+			}
+		},
+		openedMenu: {
+			x: 0,
+			transition: {
+				delay: 0.5,
+				duration: 1
+			}
+		},
+		hiddenButton: {
+			x: '-100%',
+			transition: {
+				duration: 1.5
+			}
+		},
+		showButton: {
+			x: 0,
+			transition: {
+				duration: 1,
+				delay: 0.5
+			}
+		},
+	};
+
 	return (
 		<header 
 			className={styles.header} 
@@ -39,6 +80,30 @@ export const Header = ({ ...props }: HeaderProps):JSX.Element => {
 				Войти
 			</a>
 			</div>
+			<motion.div 
+				className={styles.buttonOpen}
+				variants={variants}
+				initial="showButton"
+				animate={isOpenedMenu ? "hiddenButton" : "showButton"}
+			>
+				<ButtonIcon 
+					appearance="white" 
+					icon="burger" 
+					onClick={() => setIsOpenedMenu(true)} />
+			</motion.div>
+			<motion.div 
+				className={styles.menuMobile}
+				variants={variants}
+				initial="closedMenu"
+				animate={isOpenedMenu ? "openedMenu" : "closedMenu"}
+			>
+				<ButtonIcon 
+					className={styles.buttonClose} 
+					appearance="primary" 
+					icon="close" 
+					onClick={() => setIsOpenedMenu(false)} />
+				<Sidebar className={styles.sidebar} />
+			</motion.div>
 		</header>
 	);
 };
